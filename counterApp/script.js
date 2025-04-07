77bf2e65d039602504c4cf2d3bb1d1e1affc5fba
asadullah <asadullahjamilchaudhry@gmail.com>
2025-04-07 21:29:38 +0500

    use of innerHTML instead of innerText.

--- counterApp/script.js
+++ counterApp/script.js
@@ -1,14 +1,16 @@
 let content = document.getElementById("count-el");
 let savedContent = document.getElementById("saved-content");
 let previousEn = document.getElementById("previousEn");
-
 let count = 0;
+
 function increment() {
     count += 1;
-    content.innerText = count;
+    content.innerHTML = count;
 }
 
 function save() {
-    savedContent.innerText = "Your saved score is:" + count;
-    previousEn.innerText += count + "-";
+    savedContent.innerHTML = "Current Entry:" + count;
+    previousEn.innerHTML += count + " - ";
+    content.innerHTML = 0;
+    count = 0;
 }
