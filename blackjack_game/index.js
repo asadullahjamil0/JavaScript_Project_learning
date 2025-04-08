4c1918812f27c9e33652d03e18066bdcf6827728
asadullah <asadullahjamilchaudhry@gmail.com>
2025-04-08 17:01:54 +0500

    practising boolean expressions .

--- blackjack_game/index.js
+++ blackjack_game/index.js
@@ -1,14 +1,22 @@
 let firstCard = Math.floor(Math.random() * 10) + 2;
 let secondCard = Math.floor(Math.random() * 10) + 2;
-
+let hasBlackJack = false;
+let isAllive = true;
+let message = "";
 
 let sum = firstCard + secondCard;
 console.log(sum);
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card?");
+if (sum <= 20) {
+    message = "Do you want to draw a new card?";
 } else if (sum === 21) {
-    console.log("Whoo! You've got blackjack");
+    message = "Whoo! You've got blackjack";
+    hasBlackJack = true;
 } else {
-    console.log("You're out of game!");
+    message = "You're out of game!";
+    isAllive = true;
 }
+
+console.log(message);
+
+
