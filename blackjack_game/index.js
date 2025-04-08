3e04b5ef751e695fa166044776b17c74acc81238
asadullah <asadullahjamilchaudhry@gmail.com>
2025-04-08 17:40:24 +0500

    Added start button to start game by clicking on it and showing message and sum in paragraphs.

--- blackjack_game/index.js
+++ blackjack_game/index.js
@@ -3,20 +3,25 @@ let secondCard = Math.floor(Math.random() * 10) + 2;
 let hasBlackJack = false;
 let isAllive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
 
 let sum = firstCard + secondCard;
 console.log(sum);
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-    message = "Whoo! You've got blackjack";
-    hasBlackJack = true;
-} else {
-    message = "You're out of game!";
-    isAllive = true;
-}
+function startGame(){
 
-console.log(message);
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "Whoo! You've got blackjack";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of game!";
+        isAllive = true;
+    }   
+    messageEl.textContent = message;
+    sumEl.textContent = "Sum:" + sum;
+}
 
 
