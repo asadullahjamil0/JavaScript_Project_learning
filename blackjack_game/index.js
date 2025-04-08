f17d2bca6a1e65c37e6520352cb1b6b1243a4322
asadullah <asadullahjamilchaudhry@gmail.com>
2025-04-08 16:06:11 +0500

    blackjack game by random number and if-else statement.

---
+++ blackjack_game/index.js
@@ -0,0 +1,14 @@
+let firstCard = Math.floor(Math.random() * 10) + 2;
+let secondCard = Math.floor(Math.random() * 10) + 2;
+
+
+let sum = firstCard + secondCard;
+console.log(sum);
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card?");
+} else if (sum === 21) {
+    console.log("Whoo! You've got blackjack");
+} else {
+    console.log("You're out of game!");
+}
