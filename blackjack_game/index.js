4319249aa8d15862b46ac4d83ed5aed49b7fb6f5
asadullah <asadullahjamilchaudhry@gmail.com>
2025-04-10 16:59:15 +0500

    reassigning the value of cards and sum inside the startGame function to start game from button.

--- blackjack_game/index.js
+++ blackjack_game/index.js
@@ -1,29 +1,50 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard]
+let cards = []
 let hasBlackJack = false;
-let isAllive = true;
+let isAllive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
-let sum = cards[0] + cards[1];
+let sum = 0;
 
-function getRandomCard(){
-    return  Math.floor(Math.random() * 10) + 2;
+function rollDice() {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    return randomNumber;
+}
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+    if (randomNumber === 1) {
+        console.log(randomNumber);
+        return 11;
+    } else if (randomNumber > 10) {
+        console.log(randomNumber)
+        return 10;
+    } else {
+        return randomNumber;
+    }
 }
 
 function startGame() {
+    isAllive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards.push(firstCard);
+    cards = [firstCard , secondCard]
+    sum = cards[0] + cards[1];
+
     renderGame();
 }
 
 function renderGame() {
 
-    sumEl.innerText = `Sum:${sum}`;
-
-    for(let i = 0; i < cards.length; i++){
+    cardsEl.innerHTML = "Cards: ";
+    for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += ` ${cards[i]}`;
     }
+
+    sumEl.innerText = `Sum:${sum}`;
+
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
     } else if (sum === 21) {
@@ -31,7 +52,7 @@ function renderGame() {
         hasBlackJack = true;
     } else {
         message = "You're out of game!";
-        isAllive = true;
+        isAllive = false;
     }
     messageEl.textContent = message;
 }
@@ -40,7 +61,6 @@ function newCard() {
     let card = getRandomCard();
     cards.push(card);
     sum += cards[2];
-    cardsEl.innerHTML =  "Cards: ";
-    renderGame(); 
+    renderGame();
 }
 
