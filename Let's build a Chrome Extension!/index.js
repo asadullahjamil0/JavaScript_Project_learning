e94101135be5917523932d5e2b7720fb927f1ee0
asadullah <asadullahjamilchaudhry@gmail.com>
2025-04-14 21:39:03 +0500

    rendering elements of an array as a list items using innerHtml through javaScript.

--- Let's build a Chrome Extension!/index.js
+++ Let's build a Chrome Extension!/index.js
@@ -1,5 +1,19 @@
-let inputBtn = document.getElementById("input-btn");
+
+let myLead = ["Facebook.com", "Amazon.com", "linkedin.com"];
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+let renderBtn = document.getElementById("render-btn");
+
 inputBtn.addEventListener('click', function () {
     console.log("Button Clicked from addEventListener!");
+    myLead.push(inputEl.value);
+    console.log(myLead);
 })
+
+
+for (let i = 0; i < myLead.length; i++) {
+    ulEl.innerHTML += `<li>${myLead[i]}</li>`;
+}
+
 
