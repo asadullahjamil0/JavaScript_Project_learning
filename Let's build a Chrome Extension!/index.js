ef2f9ed27ac50b329d1ef776b397cee371268f29
asadullah <asadullahjamilchaudhry@gmail.com>
2025-04-14 22:38:15 +0500

    rendering items by just one click.

--- Let's build a Chrome Extension!/index.js
+++ Let's build a Chrome Extension!/index.js
@@ -1,30 +1,26 @@
-
-let myLead = ["Facebook.com", "Amazon.com", "linkedin.com"];
+let myLead = [];
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-let renderBtn = document.getElementById("render-btn");
 
 inputBtn.addEventListener('click', function () {
-    console.log("Button Clicked from addEventListener!");
     myLead.push(inputEl.value);
-    console.log(myLead);
+    renderFunction();
+    inputEl.value = "";
 })
 
 
-for (let i = 0; i < myLead.length; i++) {
-    // ulEl.innerHTML += `<li>${myLead[i]}</li>`;
+function renderFunction() {
+    let listItem = "";
+    for (let i = 0; i < myLead.length; i++) {
+        // ulEl.innerHTML += `<li>${myLead[i]}</li>`;
 
-    //Another way to create elements
-    const li = document.createElement("li");
-    li.textContent = myLead[i];
-    ulEl.append(li);
+        //Another way to create elements
+        // const li = document.createElement("li");
+        // li.textContent = myLead[i];
+        // ulEl.append(li);
+
+        listItem += `<li>${myLead[i]}</li>`
+    }
+    ulEl.innerHTML = listItem;
 }
-
-renderBtn.addEventListener('click', function (){
-    renderBtn.innerHTML = "Thank you for buying!";
-    
-})
-renderBtn.innerHTML = "<button>Buy</button>";
-
-
