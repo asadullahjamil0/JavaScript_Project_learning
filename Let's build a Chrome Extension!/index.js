c1a4a9aaa5fe79efd53f526ffb4228ff506ef3d5
asadullah <asadullahjamilchaudhry@gmail.com>
2025-04-15 22:58:21 +0500

    using localStorage method to store key-pair values.

--- Let's build a Chrome Extension!/index.js
+++ Let's build a Chrome Extension!/index.js
@@ -3,6 +3,10 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+// localStorage.setItem("Key", "value.com");
+localStorage.clear();
+
+
 inputBtn.addEventListener('click', function () {
     myLead.push(inputEl.value);
     renderLeads();
