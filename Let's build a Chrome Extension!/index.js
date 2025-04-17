294c061e95311b04c4c25b4ca3721de4e6d29d0f
asadullah <asadullahjamilchaudhry@gmail.com>
2025-04-17 15:13:28 +0500

    getting url from current window tab and extension is completed aslo.

--- Let's build a Chrome Extension!/index.js
+++ Let's build a Chrome Extension!/index.js
@@ -4,6 +4,7 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
+const tabBtn = document.getElementById("tab-btn");
 
 const leadFromLocalStorage = JSON.parse(localStorage.getItem("myLead"));
 if (leadFromLocalStorage) {
@@ -11,14 +12,23 @@ if (leadFromLocalStorage) {
   render(myLead);
 }
 
+tabBtn.addEventListener('click', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLead.push(tabs[0].url);
+    localStorage.setItem("myLead", JSON.stringify(myLead));
+    render(myLead);
+  })
+})
+
 function render(leads) {
   let listItems = "";
   for (let i = 0; i < leads.length; i++) {
-    listItems += `<li>
-          <a href="https://${leads[i]}" target="_blank">
-            ${leads[i]}
-          </a>
-        </li>`
+    listItems += `
+    <li>
+      <a href="${leads[i]}" target="_blank">
+        ${leads[i]}
+      </a>
+    </li>`
   }
   ulEl.innerHTML = listItems;
 }
