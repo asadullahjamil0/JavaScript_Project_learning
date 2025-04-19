e59b26cd64043b3a6187c8a977240789bffb868c
asadullah <asadullahjamilchaudhry@gmail.com>
2025-04-19 16:55:10 +0500

    Finding factorial using reduce method.

--- practise_time_part_2/find_factorial/index.js
+++ practise_time_part_2/find_factorial/index.js
@@ -1,14 +1,24 @@
 let a = 7;
 
 //Factorial using for loop
+// function factorial(a) {
+//     if (a < 0) {
+//         return `Factorial can't be calculate for negative numbers.`;
+//     }
+//     let num = 1;
+//     for (let i = 1; i <= a; i++) {
+//       num *= i;
+//     }
+//   return num;
+// }
+
+//Factorial using reduce method
 function factorial(a) {
-    if (a < 0) {
-        return `Factorial can't be calculate for negative numbers.`;
-    }
-    let num = 1;
-    for (let i = 1; i <= a; i++) {
-      num *= i;
-    }
-  return num;
+    let num = Array.from(Array(a + 1).keys());
+    let c = num.slice(1,).reduce((a, b) => {
+        return a * b;
+    })
+    console.log(c);
 }
-console.log(factorial(a))
+(factorial(a))
+
