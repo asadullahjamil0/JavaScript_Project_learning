37bfc35484d299f83831269de8b8368847938dbc
asadullah <asadullahjamilchaudhry@gmail.com>
2025-04-21 15:08:21 +0500

    Random Colour generator.

--- javaScript_dom/index.js
+++ javaScript_dom/index.js
@@ -1,13 +1,27 @@
-const boxes = document.querySelectorAll(".box");
+const getBoxes = document.querySelectorAll(".box");
+const colorBtn = document.getElementById("color-btn");
 
 //by for loop
-for (let i = 0; i < boxes.length; i++) {
-    boxes[i].style.backgroundColor = "red";
+// for (let i = 0; i < boxes.length; i++) {
+//     boxes[i].style.backgroundColor = "red";
+// }
+
+function getRandomColor() {
+    const randomNumber1 = Math.floor(Math.random() * 255) + 1;
+    const randomNumber2 = Math.floor(Math.random() * 255) + 1;
+    const randomNumber3 = Math.floor(Math.random() * 255) + 1;
+    return `rgb(${randomNumber1}, ${randomNumber2}, ${randomNumber3})`;
 }
 
 // by forEach loop
-boxes.forEach(e =>{
-    e.style.backgroundColor = "green";
-    e.style.color = "white";
-    e.style.border = "none";
+function renderBoxes() {
+    return getBoxes.forEach(e => {
+        e.style.backgroundColor = getRandomColor();
+        e.style.color = getRandomColor();
+        e.style.border = "1px solid white";
+    })
+}
+
+colorBtn.addEventListener('click', function () {
+    return renderBoxes();
 })
