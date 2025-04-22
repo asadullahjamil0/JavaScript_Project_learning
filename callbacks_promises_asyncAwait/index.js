8c856ad04df82f2ee5ae0bd32bdd8d051a097aba
asadullah <asadullahjamilchaudhry@gmail.com>
2025-04-22 22:40:51 +0500

    simple example of callback functions.

---
+++ callbacks_promises_asyncAwait/index.js
@@ -0,0 +1,10 @@
+console.log("First");
+console.log("Second");
+
+setTimeout(() => {
+    console.log("Third: Hey I am inside setTimeOut");
+
+}, 3000)
+
+console.log("Fourth");
+console.log("Fifth");
