e46f950d18f0f8b01a374fb42b9935253353afee
asadullah <asadullahjamilchaudhry@gmail.com>
2025-04-23 13:24:52 +0500

    Promise as a solution of callback hell

--- callbacks_promises_asyncAwait/index.js
+++ callbacks_promises_asyncAwait/index.js
@@ -1,35 +1,43 @@
-console.log("First");
-console.log("Second");
+// console.log("First");
+// console.log("Second");
 
-setTimeout(() => {
-    console.log("Third: Hey I am inside setTimeOut");
+// setTimeout(() => {
+//     console.log("Third: Hey I am inside setTimeOut");
 
-}, 3000)
+// }, 3000)
 
-console.log("Fourth");
-console.log("Fifth");
+// console.log("Fourth");
+// console.log("Fifth");
 
-// More in Callback functions
+// // More in Callback functions
 function getData(dataId, getNextData) {
-    setTimeout(() => {
-        console.log("Data", dataId);
-        if (getNextData) {
-            getNextData();
-        }
-    }, 2000)
+    return promise =  new Promise((resolve,reject) =>{
+        setTimeout(() => {
+            console.log("Data", dataId);
+            resolve("success")
+            if (getNextData) {
+                getNextData();
+            }
+        }, 2000)
+    })
 }
 
 //This is called callback Hell in JavaScript which we can say nested callbacks
-getData(12, () => {
-    console.log("Getting data13...");
-    getData(13, () => {
-        console.log("Getting data14...");
-        getData(14, () => {
-            console.log("Getting data15...");
-            getData(15, () => {
-                console.log("Getting data16...");
-                getData(16);
-            })
-        });
-    });
-});
+// getData(12, () => {
+//     console.log("Getting data13...");
+//     getData(13, () => {
+//         console.log("Getting data14...");
+//         getData(14, () => {
+//             console.log("Getting data15...");
+//             getData(15, () => {
+//                 console.log("Getting data16...");
+//                 getData(16);
+//             })
+//         });
+//     });
+// });
+
+// let promise = new Promise((resolve, reject) => {
+//     console.log("I am a promise");
+//     reject("Reject");
+// })
