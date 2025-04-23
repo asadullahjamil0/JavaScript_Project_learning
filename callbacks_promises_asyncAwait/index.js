e7bd576af4817b7cd31f7772e25a13b16ea43f19
asadullah <asadullahjamilchaudhry@gmail.com>
2025-04-23 13:14:59 +0500

    Callback Hell problem in a result of using several callbacks.

--- callbacks_promises_asyncAwait/index.js
+++ callbacks_promises_asyncAwait/index.js
@@ -8,3 +8,28 @@ setTimeout(() => {
 
 console.log("Fourth");
 console.log("Fifth");
+
+// More in Callback functions
+function getData(dataId, getNextData) {
+    setTimeout(() => {
+        console.log("Data", dataId);
+        if (getNextData) {
+            getNextData();
+        }
+    }, 2000)
+}
+
+//This is called callback Hell in JavaScript which we can say nested callbacks
+getData(12, () => {
+    console.log("Getting data13...");
+    getData(13, () => {
+        console.log("Getting data14...");
+        getData(14, () => {
+            console.log("Getting data15...");
+            getData(15, () => {
+                console.log("Getting data16...");
+                getData(16);
+            })
+        });
+    });
+});
