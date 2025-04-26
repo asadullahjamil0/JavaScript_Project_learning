d74f241508ae749c1d2289d5fc584b59485eb609
asadullah <asadullahjamilchaudhry@gmail.com>
2025-04-26 16:53:00 +0500

    Added blinking dots at the end of strings.

--- exercise_hacker's_terminal/script.js
+++ exercise_hacker's_terminal/script.js
@@ -1,8 +1,6 @@
 const contentEl = document.getElementById("content-el");
 let dotEl = document.getElementById("dot");
 
-const arr = ["Initializing Hacking", "Reading your files", "Password Files Detected", "Sending all passwords and personal files to server", "Cleaning up"]
-
 function getRandomDelay() {
     let timeOut = Math.floor((1 + 6 * Math.random()) * 1000);
     console.log(timeOut);
@@ -12,15 +10,33 @@ function getRandomDelay() {
 async function getData(str) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            contentEl.innerHTML += `${str}...<br>`;
+            // contentEl.innerHTML += `${str}${data}<>`;
+            let dv = document.createElement("div");
+            dv.innerHTML = str;
+            document.body.append(dv);
+
             resolve("Success")
         }, getRandomDelay())
     })
 }
 
 async function displayData() {
+    const arr = ["Initializing Hacking", "Reading your files", "Password Files Detected", "Sending all passwords and personal files to server", "Cleaning up"]
+
     for (let i = 0; i < arr.length; i++) {
         await getData(arr[i]);
+        let t = setInterval(() => {
+            let last = document.body.querySelectorAll("div");
+            last = last[last.length - 1]
+            if (last.innerHTML.endsWith("...")) {
+                last.innerHTML = last.innerHTML.slice(0, last.innerHTML.length - 1);
+            } else {
+                last.innerHTML = last.innerHTML + ".";
+            }
+        }, 500);
+        console.log(t);
+        
     }
+    clearInterval();
 }
 displayData();
