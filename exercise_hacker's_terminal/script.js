808198d35976f37258e2e80df2de57e70831c6ea
asadullah <asadullahjamilchaudhry@gmail.com>
2025-04-26 15:34:24 +0500

    minor addition

--- exercise_hacker's_terminal/script.js
+++ exercise_hacker's_terminal/script.js
@@ -1,20 +1,26 @@
 const contentEl = document.getElementById("content-el");
+let dotEl = document.getElementById("dot");
 
-const arr = ["Initializing Hacking...", "Reading your files...", "Password Files Detected...", "Sending all passwords and personal files to server...", "Cleaning up..."]
+const arr = ["Initializing Hacking", "Reading your files", "Password Files Detected", "Sending all passwords and personal files to server", "Cleaning up"]
+
+function getRandomDelay() {
+    let timeOut = Math.floor((1 + 6 * Math.random()) * 1000);
+    console.log(timeOut);
+    return timeOut;
+}
 
 async function getData(str) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            contentEl.innerHTML += `${str}<br>`;
+            contentEl.innerHTML += `${str}...<br>`;
             resolve("Success")
-        }, 3000)
+        }, getRandomDelay())
     })
 }
 
 async function displayData() {
     for (let i = 0; i < arr.length; i++) {
-        let data = await getData(arr[i]);
+        await getData(arr[i]);
     }
 }
-
 displayData();
