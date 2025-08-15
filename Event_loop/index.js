85079546a65a4a80502188f38dd36297715c48a0
asadullahjamil0 <asadullahjamilchaudhry@gmail.com>
2025-08-15 22:41:41 +0500

    The event loop and concurrency in javaScript.

---
+++ Event_loop/index.js
@@ -0,0 +1,5 @@
+console.log("Start");
+setTimeout(() => console.log("Timeout"), 1000);
+Promise.resolve().then(() => console.log("Promise"));
+console.log("End");
+// Output: Start, End, Promise, Timeout
